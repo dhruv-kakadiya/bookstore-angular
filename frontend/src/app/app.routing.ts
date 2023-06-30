8092c9941283c3e607fbd891f672c0e752b2f43e
Dhruv-Kakadiya <kakadiya.1@iitj.ac.in>
2023-07-01 00:54:44 +0530

    Cart Setup

--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { CatalogComponent } from './catalog/catalog.component';
+import { CartComponent } from './cart/cart.component';
 
 
 const routes: Routes = [
     { path: '', component: HomepageComponent },
     { path: 'catalog', component: CatalogComponent },
+    { path: 'cart', component: CartComponent },
 ];
 
 @NgModule({
